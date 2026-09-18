646638bf0e071658fcea477eef48427cb74a4c9c
Big <162275628+DopestT@users.noreply.github.com>
2026-09-18 17:29:26 -0400

    Enable Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App'
 import { TokenEfficiencyPanel } from './TokenEfficiencyPanel'
 import './styles.css'
@@ -8,5 +9,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
     <TokenEfficiencyPanel />
+    <Analytics />
   </React.StrictMode>,
 )
